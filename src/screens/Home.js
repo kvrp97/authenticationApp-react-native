0dfd176094e2a4b6faa1b92433386d11282ae6ca
kvrp97 <ravinduprabhashwara97@gmail.com>
2023-05-15 00:41:55 +0530

    Home done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,54 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Button from '../components/Button';
 
-const Home = () => {
+const Home = ({ navigation }) => {
+  const [userDetails, setUserDetails] = useState();
+
+  useEffect(() => {
+    getUserDetails();
+  }, [])
+
+  const getUserDetails = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('user');
+      if (userData) {
+        setUserDetails(JSON.parse(userData));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const logOut = () => {
+    AsyncStorage.setItem('user', JSON.stringify({ ...userDetails, loggedIn: false }));
+    navigation.navigate('LogIn');
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.welcomeText}>Welcome {userDetails?.fullName}</Text>
+      <Button title={'LogOut'} onPress={logOut} />
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 40
+  },
+  welcomeText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10
+  }
+})
 
 
 export default Home
